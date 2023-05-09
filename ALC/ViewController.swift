5259c1348e3a328af42611e32eb80c5788f6effc
dotRedCoding <68290475+dotRedCoding@users.noreply.github.com>
2023-05-09 09:33:31 -0400

    - played with some variations of ways to set the height for the labels. The final version is like the third revision and the easiest way to approach it but for review of all the different methods we would need to rewatch part 4 of the video

--- ALC/ViewController.swift
+++ ALC/ViewController.swift
@@ -60,9 +60,10 @@ class ViewController: UIViewController {
             // visual format in stirng form describing how we want our labels to look+
         }
         
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
-       
+        let metrics = ["labelHeight": 88]
         
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary)) // when specifying a space you need to add a dash on each side -(size)-
+
         
     }
     
